0b6375ad871dbd199e2a3dec5281aa6ba659ff03
FabioRagosta <53743894+FabioRagosta@users.noreply.github.com>
2022-06-24 18:51:27 +0200

    Update TransientsPM.py

--- pmAnom/TransientsPM.py
+++ pmAnom/TransientsPM.py
@@ -1,12 +1,8 @@
 import sys,os, glob, time, astropy, warnings, pickle
 import numpy as np
 import matplotlib.pyplot as plt
-from shapely.geometry import Point, Polygon
 import pandas as pd
-from scipy.stats import *
-from astropy.stats import histogram
 from astropy.io import fits
-import sklearn.mixture.gaussian_mixture as GMM
 from builtins import zip
 ### LSST dependencies 
 from lsst.sims.maf.metrics import BaseMetric
@@ -24,12 +20,6 @@ from opsimUtils import *
 
 
 class TransienPM(BaseMetric): 
-    def sigma_slope_arr(x, sigmay):
-            w = 1./np.array(sigmay)**2
-            denom = np.sum(x)*np.sum(w*x**2,axis=1)-np.sum(w*x,axis=1)**2
-            denom[np.where(denom <= 0)]=np.nan
-            denom[np.where(denom > 0)] = np.sqrt(np.sum(w)*denom[np.where(denom > 0)]**-1 )
-            return denom
      #    Generate a population of transient objects and see what is its proper motion  , 
     def __init__(self, metricName='TransienPM', f='g', snr_lim=5,m5Col='fiveSigmaDepth',  
                   mjdCol='observationStartMJD',filterCol='filter',seeingCol='seeingFwhmGeom', surveyduration=10, **kwargs): 
@@ -40,7 +30,7 @@ class TransienPM(BaseMetric):
             self.snr_lim = snr_lim 
             self.f = f
             self.surveyduration = surveyduration  
-            sim = pd.read_csv('/home/idies/workspace/Storage/fragosta/persistent/LSST_OpSim/Scripts_NBs/True Novelities/simulation_pm.csv', usecols=['MAG','MODE','d','PM','PM_out'])
+            sim = pd.read_csv('/home/idies/workspace/Storage/fragosta/persistent/LSST_OpSim/Scripts_NBs/simulation_pm.csv', usecols=['MAG','MODE','d','PM','PM_out'])
             self.simobj = sim
             super(TransienPM, self).__init__(col=[self.mjdCol, self.m5Col,self.seeingCol, self.filterCol], 
                                                        units='Fraction Detected', 
@@ -59,11 +49,18 @@ class TransienPM(BaseMetric):
      #            Initial time (mjd) , 
      #        m_r_0 : float , 
      #            initial r-band brightness (mags) , 
-             lightcurve = np.zeros(np.size(t), dtype=float) + 99. 
+            #lightcurve = np.zeros((np.size(t), np.size(t0)), dtype=float) + 99.
+            T_matrix=np.ones((np.size(t0),np.size(t)))*t
+            T0s_matrix=np.ones((np.size(t),np.size(t0)))*t0
+            M = np.ones((np.size(t),np.size(t0)))*peak
+            S = np.ones((np.size(t),np.size(t0)))*slope
      # Select only times in the lightcurve duration window , 
-             good = np.where( (t >= t0) & (t <= t0+duration) ) 
-             lightcurve[good] = peak + slope*(t[good]-t0) 
-             return lightcurve 
+            T_good=np.where((T_matrix>=T0s_matrix.T) & (T_matrix<=np.array(T0s_matrix+duration).T),T_matrix,0) 
+            T0s_good=np.where((T_matrix.T>=T0s_matrix) & (T_matrix.T<=np.array(T0s_matrix+duration)),T0s_matrix,0)
+            M_good=np.where((T_matrix.T>=T0s_matrix) & (T_matrix.T<=np.array(T0s_matrix+duration)),M,0)
+            S_good=np.where((T_matrix.T>=T0s_matrix) & (T_matrix.T<=np.array(T0s_matrix+duration)),S,0)
+            lightcurve = M_good.T + S_good.T*(T_good-T0s_good.T) 
+            return lightcurve 
       
     def run(self,  dataSlice, slicePoint=None): 
             pm = np.array(self.simobj['PM_out'])
@@ -75,21 +72,18 @@ class TransienPM(BaseMetric):
             if (self.f in flt):
                 snr = m52snr(mag[:, np.newaxis],dataSlice[self.m5Col][obs])
                 row, col =np.where(snr>self.snr_lim)
-                precis = astrom_precision(dataSlice[self.seeingCol][obs], snr)
-                sigmapm=sigma_slope_arr(dataSlice[self.mjdCol][obs], precis)*365.25*1e3
+                
+                Times = np.sort(mjd)
+                dt = np.array(list(combinations(Times,2)))
+                if np.size(dt)>0:
+                    DeltaTs = np.absolute(np.subtract(dt[:,0],dt[:,1]))            
+                    DeltaTs = np.unique(DeltaTs)
 
-                #select the objects which displacement can be detected
-                Times = list(mjd)
-                DeltaTs = []
-                while np.size(Times)>1:
-                    for d in range(len(Times)-1):
-                        DeltaTs.append(Times[d]-Times[d+1])
-                    Times.remove(Times[0])
-                dt_pm = 0.05*np.median(dataSlice[self.seeingCol])/pm[np.unique(row)]
-                DeltaTs.sort()
-                DeltaTs = np.array(DeltaTs)
-                if np.size(DeltaTs)>0:
-                    selection = np.where((dt_pm>DeltaTs[0]) & (dt_pm<DeltaTs[-1]))
+                    dt_pm = 0.05*np.amin(dataSlice[self.seeingCol])/pm[np.unique(row)]
+                    selection = np.where((dt_pm>min(DeltaTs)) & (dt_pm<max(DeltaTs)))
+
+                #precis = astrom_precision(dataSlice[self.seeingCol][obs], snr[row,:])
+                #sigmapm= sigma_slope(dataSlice[self.mjdCol][obs], precis)*365.25*1e3
 
                     objRate = 0.7 # how many go off per day
                     nObj=np.size(pm[selection])
@@ -99,20 +93,26 @@ class TransienPM(BaseMetric):
              # Loop though each generated transient and decide if it was detected , 
              # This could be a more complicated piece of code, for example demanding  , 
              # A color measurement in a night. , 
-
-                    for i,t0 in enumerate(np.random.uniform(0,self.surveyduration,nObj)): 
-                        duration =dt_pm[selection][i]
-                        slope = np.random.uniform(-3,3) 
-                        lc = self.lightCurve(t, t0, m0s[i],duration, slope) 
-                        good = m52snr(lc,dataSlice[self.m5Col][obs])> self.snr_lim 
-                        detectTest = dataSlice[self.m5Col][obs] - lc 
-                        if detectTest.max() > 0 and len(good)>2: 
-                             detected += 1 
+                    durations = dt_pm[selection]
+                    slopes = np.random.uniform(-3,3,np.size(selection))
+                    t0s = np.random.uniform(0,self.surveyduration,nObj)
+                    lcs = self.lightCurve(t, t0s, m0s,durations, slopes) 
+                    good = m52snr(lcs,dataSlice[self.m5Col][obs])> self.snr_lim
+                    detectedTest = good.sum(axis=0)
+                    detected = np.sum(detectedTest>2)
+                    #for i,t0 in enumerate(np.random.uniform(0,self.surveyduration,nObj)): 
+                    #    duration =dt_pm[selection][i]
+                    #    slope = np.random.uniform(-3,3) 
+                    #    lc = self.lightCurve(t, t0, m0s[i],duration, slope) 
+                    #    good = m52snr(lc,dataSlice[self.m5Col][obs])> self.snr_lim 
+                    #    detectTest = dataSlice[self.m5Col][obs] - lc 
+                    #    if detectTest.max() > 0 and len(good)>2: 
+                    #         detected += 1 
                      # Return the fraction of transients detected , 
                     if float(nObj) == 0:
                         A = np.inf 
                     else: 
                         A=float(nObj) 
-                        res = float(np.sum(detected))/A            
+                        res = float(detected)/A            
                         #print('detected fraction:{}'.format(res)) 
                         return res
