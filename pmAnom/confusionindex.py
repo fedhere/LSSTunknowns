f485693c32abe6bd117915067dee07f5dee12fc7
FabioRagosta <53743894+FabioRagosta@users.noreply.github.com>
2022-06-24 18:48:39 +0200

    Update confusionindex.py

--- pmAnom/confusionindex.py
+++ pmAnom/confusionindex.py
@@ -1,13 +1,11 @@
 import sys,os, glob, time, astropy, warnings, pickle
 import numpy as np
 import matplotlib.pyplot as plt
-from shapely.geometry import Point, Polygon
 import pandas as pd
-from scipy.stats import *
-from astropy.stats import histogram
 from astropy.io import fits
 import sklearn.mixture.gaussian_mixture as GMM
 from builtins import zip
+from itertools import combinations
 ### LSST dependencies 
 from lsst.sims.maf.metrics import BaseMetric
 import lsst.sims.maf.slicers as slicers
@@ -21,49 +19,21 @@ from opsimUtils import *
 
 
 
-class reducedPM(BaseMetric): 
-        def sigma_slope_arr(x, sigmay):
-            w = 1./np.array(sigmay)**2
-            denom = np.sum(x)*np.sum(w*x**2,axis=1)-np.sum(w*x,axis=1)**2
-            denom[np.where(denom <= 0)]=np.nan
-            denom[np.where(denom > 0)] = np.sqrt(np.sum(w)*denom[np.where(denom > 0)]**-1 )
-            return denom
-    
-        def readfile(self, filename='', colsname=['']): 
-            if 'csv' in filename: 
-                #print('reading {}'.format(filename)) 
-                data = pd.read_csv(filename, header=0, names = colsname ) 
-            elif 'fits' in filename: 
-                 #print('reading {}'.format(filename)) 
-                hdul = fits.open(filename) 
-                data = hdul[1].data 
-            elif ['txt', 'dat'] in filename: 
-                #print('reading {}'.format(filename)) 
-                data = {k:[] for k in colsname} 
-                f = open(filename) 
-                righe = f.readlines() 
-                for line in righe: 
-                    line.split() 
-                    for i, k in enumerate(colsname): 
-                        data[k].append(float(line[i]))                 
-            elif 'json'in filename: 
-                print('not implemented to read .json extention') 
-            return data 
-        
-        def __init__(self, filename = 'Carlin12.csv', snr_lim=5,mode=None, MagIterLim=[0,1,1], surveyduration=10,
-                      metricName='reducedPM',m5Col='fiveSigmaDepth',real_data= True, out_type = 'confusion',
+class reducedPM(BaseMetric):         
+        def __init__(self, fits_filename = 'sa93.all.fits', snr_lim=5,mode=None, MagIterLim=[0,1,1], surveyduration=10,
+                      metricName='reducedPM',m5Col='fiveSigmaDepth',gap_selection=False,real_data= True,
                       mjdCol='observationStartMJD',filterCol='filter', seeingCol='seeingFwhmGeom',dataout=True,**kwargs): 
             self.mjdCol = mjdCol 
             self.m5Col = m5Col 
             self.seeingCol = seeingCol 
             self.filterCol = filterCol 
             self.snr_lim = snr_lim 
-            self.filename = filename  
+            self.fits_filename = fits_filename  
             self.dataout = dataout 
             self.mode = mode 
+            self.gap_selection=gap_selection
             self.MagIterLim = MagIterLim 
             self.surveyduration = surveyduration 
-            self.out_type = out_type
             self.real_data = real_data
              # to have as output all the simulated observed data set dataout=True, otherwise the relative error for  
              # each helpix is estimated 
@@ -74,20 +44,38 @@ class reducedPM(BaseMetric):
                 super(reducedPM, self).__init__(col=[self.mjdCol,self.filterCol,  self.m5Col,self.seeingCol, 'night' ], 
                                                             units='', metricDtype='float',metricName=metricName, 
                                                              **kwargs) 
-            if self.real_data:    
-                colsname=['RA', 'DEC','g','g-r','Hg','PM_OUT','deltaX'] 
-                self.data = self.readfile(self.filename, colsname)
-                self.data['MAG']=self.data['g']
-            else:
-                colsname=['MAG','MODE','d','PM','PM_OUT']
-                self.data = self.readfile(self.filename, colsname)
-            data_sag93 = fits.open('sa93.all.fits')
+            
+            data_sag93 = fits.open(self.fits_filename)
             table = data_sag93[1].data    
-            mu_sag= np.transpose([table['MUX'],table['MUY']])
-            self.mu_sag=mu_sag
+            mu_sag= np.transpose(np.sqrt(table['MUX']**2+table['MUY']**2))
+            M_sag = np.transpose(table['GMAG'])
+            self.mu_sag = mu_sag
+            self.mag_sag = M_sag
+            self.gr = np.transpose(table['GMAG']-table['RMAG'])
+        def sigma_slope(self,x, sigma_y):
+            """
+            Calculate the uncertainty in fitting a line, as
+            given by the spread in x values and the uncertainties
+            in the y values.
+
+            Parameters
+            ----------
+            x : numpy.ndarray
+                The x values of the data
+            sigma_y : numpy.ndarray
+                The uncertainty in the y values
+
+            Returns
+            -------
+            float
+                The uncertainty in the line fit
+            """
+            w = 1/sigma_y*1/sigma_y
+            denom = np.sum(w)*np.einsum('i,ij->j',x**2,w.T)-np.einsum('i,ij->j',x,w.T)**2
+            select= np.where(denom > 0)
+            res=np.sqrt(np.sum(w,axis=1)[select]/denom[select] )*365.25*1e3
+            return res                   
         def run(self, dataSlice, slicePoint=None): 
-            pm = np.array(self.data['PM_OUT'])
-            mag = np.array(self.data['MAG'])
             obs = np.where(dataSlice[self.mjdCol]<min(dataSlice[self.mjdCol])+365*self.surveyduration)
             
             deltamag= np.arange(self.MagIterLim[0],self.MagIterLim[1],self.MagIterLim[2])
@@ -95,9 +83,11 @@ class reducedPM(BaseMetric):
             for dm in deltamag: 
                 
                     if self.mode == 'distance': 
-                        pmnew= pm/(10**(dm/5)) 
+                        pmnew= self.mu_sag /(10**(dm/5)) 
+                        mag = self.mag_sag + dm
                     elif self.mode == 'density': 
-                        pmnew= pm 
+                        pmnew= self.mu_sag  
+                        mag = self.mag_sag + dm
                     else: 
                         print('##### ERROR: the metric is not implemented for this mode.')
                         
@@ -108,62 +98,36 @@ class reducedPM(BaseMetric):
                         # select objects above the limit magnitude threshold 
                         snr = m52snr(mag[:, np.newaxis],dataSlice[self.m5Col][obs])
                         row, col =np.where(snr>self.snr_lim)
-                        precis = astrom_precision(dataSlice[self.seeingCol][obs], snr)
-                        sigmapm=sigma_slope_arr(dataSlice[self.mjdCol][obs], precis)*365.25*1e3
-                           
-                        #select the objects which displacement can be detected
-                        Times = list(mjd)
-                        DeltaTs = []
-                        while np.size(Times)>1:
-                            for d in range(len(Times)-1):
-                                DeltaTs.append(Times[d]-Times[d+1])
-                            Times.remove(Times[0])
-                        DeltaTs.sort()
-                        DeltaTs = np.array(DeltaTs)
-                        if np.size(DeltaTs)>0:
-                            dt_pm = 0.05*np.median(dataSlice[self.seeingCol])/pmnew[np.unique(row)]
-                            selection = np.where((dt_pm>DeltaTs[0]) & (dt_pm<DeltaTs[-1]))
-
-                            if self.real_data:
-                                Hg = np.array(self.data['Hg'])[selection]
-                                gr = np.array(self.data['g-r'])[selection] 
-                            else:
-                                colsname=['RA', 'DEC','MAG','g-r','Hg','PM_OUT','deltaX'] 
-                                color_dist = self.readfile('data.csv', colsname)
-                                gr = np.random.choice(color_dist['g-r'],size= np.size(selection))
-                                Hg = mag[selection]+5*np.log10(pm[selection])-10
-
-                            g= mag+dm 
-                            sigmaHg = np.sqrt((g[selection]/m52snr(g[selection],np.median(dataSlice[self.m5Col])))**(2)+ (4.715*sigmapm[selection]/pmnew[selection])**2) 
-                            sigmag = np.sqrt((g[selection]/m52snr(g[selection],np.median(dataSlice[self.m5Col])))**2+((g[selection]-gr)/m52snr((g[selection]-gr),np.median(dataSlice[self.m5Col])))**2)
-                            err_ellipse = np.pi*sigmaHg*sigmag
-                            n = len(pmnew[selection])/len(pmnew)
-                            pa= np.random.uniform(0,2*np.pi,len(pmnew[selection]))
-                            n_components = np.arange(1, 10)
-                            pm_alpha = pmnew[selection]*np.sin(pa)
-                            pm_delta = pmnew[selection]*np.cos(pa)
-                            mu = np.transpose(np.array([pm_alpha,pm_delta]))
-                            Phi_c = np.exp(((mu[:,0]-0.42)**2+(mu[:,1]+2.66)**2)/2)
-                            gamma=((0.42-np.median(self.mu_sag[:,0]))*(-2.66-np.median(self.mu_sag[:,0])))
-                            Phi_f=np.exp(((mu[:,0]-np.median(self.mu_sag[:,0]))**2-(mu[:,0]-np.median(self.mu_sag[:,0]))*(mu[:,1]-np.median(self.mu_sag[:,1]))+(mu[:,1]-np.median(self.mu_sag[:,1]))**2)/2/(1-gamma))
-                            P=Phi_c/Phi_f
-                            idx_c =np.where(P>1)
-
-                            try:
-                                models = [GMM.GaussianMixture(n, covariance_type='full', random_state=0).fit(mu[idx_c]) for n in n_components]
-                                BIC = [m.bic(mu[idx_c]) for m in models]
-                                k_cluster=n_components[np.where(BIC==np.min(BIC))][0]    
-                            except:
-                                k_cluster = 0
-                            out[dm] = {'CI':np.median(err_ellipse), 'k': k_cluster, 'PM':mu}  
+                        if self.gap_selection:
+                            Times = np.sort(mjd)
+                            dt = np.array(list(combinations(Times,2)))
+                            DeltaTs = np.absolute(np.subtract(dt[:,0],dt[:,1]))            
+                            DeltaTs = np.unique(DeltaTs)
+                            if np.size(DeltaTs)>0:
+                                         dt_pm = 0.05*np.amin(dataSlice[self.seeingCol])/pmnew[np.unique(row)]
+                                         selection = np.where((dt_pm>min(DeltaTs)) & (dt_pm<max(DeltaTs)))
                         else:
-                            out[dm] = {'CI':0, 'k': 0, 'PM':pm} 
+                            selection = np.unique(row)
+                        precis = astrom_precision(dataSlice[self.seeingCol][obs], snr[row,:])
+                        sigmapm= self.sigma_slope(dataSlice[self.mjdCol][obs], precis)
+                        Hg = mag[selection]+5*np.log10(pmnew[selection])-10
+                        sigmaHg = np.sqrt((mag[selection]/m52snr(mag[selection],np.median(dataSlice[self.m5Col])))**(2)+ (4.715*sigmapm[selection]/np.ceil(pmnew[selection]))**2) 
+                        sigmag = np.sqrt((mag[selection]/m52snr(mag[selection],np.median(dataSlice[self.m5Col])))**2+((mag[selection]-self.gr[selection])/m52snr((mag[selection]-self.gr[selection]),np.median(dataSlice[self.m5Col])))**2)
+                        err_ellipse = np.pi*sigmaHg*sigmag
+                        if self.dataout:
+                            CI = np.array([np.nansum((([gr-gcol ])/sigmag)**2 + ((Hg-h)/sigmaHg)**2 <= 1)/np.size(pmnew[selection]) for (gcol,h) in zip(gr,Hg)])                      
+
+                            out[dm] = {'CI':CI,'alpha':np.size(pmnew[selection])/np.size(pmnew) ,'err':err_ellipse,'Hg':Hg,'gr':gr, 'sigmaHg':sigmaHg,'sigmagr':sigmag}
+                        else:
+                            out[dm] = {'alpha':np.size(pmnew[selection])/np.size(pmnew) ,'err':err_ellipse}
+                    else:
+                        if self.dataout:
+                            out[dm] = {'CI':0,'alpha':0,'err':0,'Hg':Hg,'gr':gr, 'sigmaHg':sigmaHg,'sigmagr':sigmag} 
+                        else:
+                            out[dm] = {'alpha':0 ,'err':0}
             if self.dataout: 
                 return out  
             else:
                 if ('g' in flt) and ('r' in flt):
-                    if self.out_type =='cluster':
-                        res = out[dm]['k']
-                    elif self.out_type == 'confusion':
-                        res = out[dm]['CI']*np.size(selection)/np.size(pm)
-                    return res
+                    res = out[dm]['alpha']/np.nanmean(out[dm]['err'][np.isfinite(out[dm]['err'])])                    
+                    return res 
