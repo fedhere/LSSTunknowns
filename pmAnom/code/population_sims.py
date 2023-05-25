4677d0995537f03d7509051196a16b1f91c18648
FabioRagosta <53743894+FabioRagosta@users.noreply.github.com>
2023-05-25 12:54:10 +0200

    Update population_sims.py

--- pmAnom/code/population_sims.py
+++ pmAnom/code/population_sims.py
@@ -68,55 +68,56 @@ def sims_pm(slicers, nside, templatefile):
     population['pm_un_dec']= []
     population['mag'] = []
     population['galcomp'] = []
+    distance = np.random.uniform(0.004,12,5000) *1000 # pc
     for i in np.unique(gaia_pix):
-      sample_pix = np.where(gaia_pix == i)
-      population['ra'].append(np.random.choice(gaia_ra[sample_pix].value,5000))
-      population['dec'].append(np.random.choice(gaia_dec[sample_pix].value,5000))
-      x,y,z = galactic_x,galactic_y,galactic_z
-      gcs = Galactic(u=x[sample_pix]*u.pc, v=y[sample_pix]*u.pc, w=z[sample_pix]*u.pc, 
-                    U=gaia['vx'].to_numpy()[sample_pix]*u.km/u.s,V=gaia['vy'].to_numpy()[sample_pix]*u.km/u.s, W=gaia['vz'].to_numpy()[sample_pix]*u.km/u.s,
-                    representation_type=CartesianRepresentation, 
-                    differential_type=CartesianDifferential)
-      gcs = gcs.transform_to(ICRS)
-      pm_ra_cosdec, pm_dec = gcs.proper_motion.value
+        sample_pix = np.where(gaia_pix == i)
+        population['ra'].append(np.random.choice(gaia_ra[sample_pix].value,5000))
+        population['dec'].append(np.random.choice(gaia_dec[sample_pix].value,5000))
+        x,y,z = galactic_x,galactic_y,galactic_z
+        gcs = Galactic(u=x[sample_pix]*u.pc, v=y[sample_pix]*u.pc, w=z[sample_pix]*u.pc, 
+                      U=gaia['vx'].to_numpy()[sample_pix]*u.km/u.s,V=gaia['vy'].to_numpy()[sample_pix]*u.km/u.s, W=gaia['vz'].to_numpy()[sample_pix]*u.km/u.s,
+                      representation_type=CartesianRepresentation, 
+                      differential_type=CartesianDifferential)
+        gcs = gcs.transform_to(ICRS)
+        pm_ra_cosdec, pm_dec = gcs.proper_motion.value
 
-      dH, dbin = np.histogram(gaia_distance[sample_pix])
-      dprobabilities = dH / np.sum(dH)
-      d_random_index = np.random.choice(range(len(dprobabilities)), size=5000, p=dprobabilities)
-      d_sample = dbin[d_random_index]
-      v_unusual = np.random.uniform(-500, 500 ,size=(5000,3))
-      catalog_target = SkyCoord(ra=np.mean(gaia_ra[sample_pix].value)*u.degree, 
-                                dec=np.mean(gaia_dec[sample_pix].value)*u.degree, distance=d_sample*u.pc)
-      cc = catalog_target.transform_to(ICRS)
-      x_unusual = cc.cartesian.x.value/1000
-      y_unusual = cc.cartesian.y.value/1000
-      z_unusual = cc.cartesian.z.value/1000
-      gcs = Galactic(u=x_unusual*u.kpc, v=y_unusual*u.kpc, w=z_unusual*u.kpc, 
-                  U=v_unusual[:,0]*u.km/u.s,V=v_unusual[:,1]*u.km/u.s, W=v_unusual[:,2]*u.km/u.s,
-                  representation_type=CartesianRepresentation, differential_type=CartesianDifferential)
-      gcs = gcs.transform_to(ICRS)
-      pm_un_ra_cosdec ,pm_un_dec = gcs.proper_motion.value
-      population['pm_un_ra_cosdec'].append(pm_un_ra_cosdec) 
-      population['pm_un_dec'].append(pm_un_dec)
-
-      H, pm = np.histogram(np.sqrt(pm_ra_cosdec**2 + pm_dec**2))
-      probabilities = H / np.sum(H)
-
-      # Randomly sample from the histogram using numpy.random.choice()
-      random_index = np.random.choice(range(len(probabilities)), size=5000, p=probabilities)
-      population['pm_ra_cosdec'].append(pm_ra_cosdec[random_index-1])
-      population['pm_dec'].append(pm_dec[random_index-1])
-      Bulge_magdist = np.random.choice(ascii.read('./Bulge.isc_sloan')['col6'].data, size=5000)
-      Disk_magdist = np.random.choice(ascii.read('./Disk.isc_sloan')['col6'].data, size=5000)
-      Halo_magdist = np.random.choice(ascii.read('./Halo.isc_sloan')['col6'].data, size=5000)
-      total_sample_m = np.empty(5000)
-      total_sample_m[d_sample/8.2 < 0.3]=Bulge_magdist[d_sample/8.2 < 0.3]
-      total_sample_m[(d_sample/8.2 > 0.3) & (z_unusual/8.2<0.3)]=Disk_magdist[(d_sample/8.2 > 0.3) & (z_unusual/8.2<0.3)]
-      total_sample_m[(d_sample/8.2 > 0.3) & (z_unusual/8.2>0.3)]=Halo_magdist[(d_sample/8.2 > 0.3) & (z_unusual/8.2>0.3)]
-      population['mag'].append(total_sample_m+5*np.log10(distance)+5)
-      comp = np.empty(5000,dtype=str)
-      comp[d_sample/8.2 < 0.3] = ['B']*np.sum(d_sample/8.2 < 0.3)
-      comp[(d_sample/8.2 > 0.3) & (z_unusual/8.2<0.3)] = ['D']*np.sum((d_sample/8.2 > 0.3) & (z_unusual/8.2<0.3))
-      comp[(d_sample/8.2 > 0.3) & (z_unusual/8.2>0.3)] = ['H']*np.sum((d_sample/8.2 > 0.3) & (z_unusual/8.2>0.3))
-      population['galcomp'].append(comp)
+        dH, dbin = np.histogram(gaia_distance[sample_pix])
+        dprobabilities = dH / np.sum(dH)
+        d_random_index = np.random.choice(range(len(dprobabilities)), size=5000)
+        d_sample = dbin[d_random_index]
+        v_unusual = np.random.uniform(-500, 500 ,size=(5000,3))
+        catalog_target = SkyCoord(ra=np.mean(gaia_ra[sample_pix].value)*u.degree, 
+                                  dec=np.mean(gaia_dec[sample_pix].value)*u.degree, distance=d_sample*u.pc)
+        cc = catalog_target.transform_to(ICRS)
+        x_unusual = cc.cartesian.x.value/1000
+        y_unusual = cc.cartesian.y.value/1000
+        z_unusual = cc.cartesian.z.value/1000
+        gcs_un = Galactic(u=x_unusual*u.kpc, v=y_unusual*u.kpc, w=z_unusual*u.kpc, 
+                    U=v_unusual[:,0]*u.km/u.s,V=v_unusual[:,1]*u.km/u.s, W=v_unusual[:,2]*u.km/u.s,
+                    representation_type=CartesianRepresentation, differential_type=CartesianDifferential)
+        gcs_un = gcs_un.transform_to(ICRS)
+        pm_un_ra_cosdec ,pm_un_dec = gcs_un.proper_motion.value
+        population['pm_un_ra_cosdec'].append(pm_un_ra_cosdec) 
+        population['pm_un_dec'].append(pm_un_dec)
+        v_gaia = np.column_stack([gaia['vx'].to_numpy()[sample_pix],gaia['vy'].to_numpy()[sample_pix],gaia['vz'].to_numpy()[sample_pix]])
+        v_usual = v_gaia[np.random.choice(range(v_gaia.shape[0]), size=5000)]
+        gcs = Galactic(u=x_unusual*u.kpc, v=y_unusual*u.kpc, w=z_unusual*u.kpc, 
+                    U=v_usual[:,0]*u.km/u.s,V=v_usual[:,1]*u.km/u.s, W=v_usual[:,2]*u.km/u.s,
+                    representation_type=CartesianRepresentation, differential_type=CartesianDifferential)
+        pm_ra_cosdec ,pm_dec = gcs.proper_motion.value
+        population['pm_ra_cosdec'].append(pm_ra_cosdec) 
+        population['pm_dec'].append(pm_dec)
+        Bulge_magdist = np.random.choice(ascii.read('./Bulge.isc_sloan')['col6'].data, size=5000)
+        Disk_magdist = np.random.choice(ascii.read('./Disk.isc_sloan')['col6'].data, size=5000)
+        Halo_magdist = np.random.choice(ascii.read('./Halo.isc_sloan')['col6'].data, size=5000)
+        total_sample_m = np.empty(5000)
+        total_sample_m[d_sample/8.2 < 0.3]=Bulge_magdist[d_sample/8.2 < 0.3]
+        total_sample_m[(d_sample/8.2 > 0.3) & (z_unusual/8.2<0.3)]=Disk_magdist[(d_sample/8.2 > 0.3) & (z_unusual/8.2<0.3)]
+        total_sample_m[(d_sample/8.2 > 0.3) & (z_unusual/8.2>0.3)]=Halo_magdist[(d_sample/8.2 > 0.3) & (z_unusual/8.2>0.3)]
+        population['mag'].append(total_sample_m+5*np.log10(distance)+5)
+        comp = np.empty(5000,dtype=str)
+        comp[d_sample/8.2 < 0.3] = ['B']*np.sum(d_sample/8.2 < 0.3)
+        comp[(d_sample/8.2 > 0.3) & (z_unusual/8.2<0.3)] = ['D']*np.sum((d_sample/8.2 > 0.3) & (z_unusual/8.2<0.3))
+        comp[(d_sample/8.2 > 0.3) & (z_unusual/8.2>0.3)] = ['H']*np.sum((d_sample/8.2 > 0.3) & (z_unusual/8.2>0.3))
+        population['galcomp'].append(comp)
     return population
